2b6810808770cb89fe208b4a9d04bdaafb585f22
Alex <aleksanderbabi4eff@yandex.ru>
2020-12-12 20:16:19 +0300

    some changes

--- src/app/components/fill-model/fill-model.component.ts
+++ src/app/components/fill-model/fill-model.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FillModelComponent implements OnInit {
 
+  newModels: number = 0;
+
   constructor() { }
+
+  addModel(){
+    if(this.newModels<6){
+      this.newModels++;
+    }
+  }
+
+  deleteModel(){
+    if(this.newModels>0){
+      this.newModels--;
+    }
+  }
 
   ngOnInit(): void {
   }
